1c204b72a4be121bfb3d83dd996fdc3b2b357010
Jenna Dean <jenna.dean9@gmail.com>
2019-11-20 11:53:24 -0500

    css on some header, user login, and button

--- src/App.js
+++ src/App.js
@@ -1,38 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import {Route, Link} from 'react-router-dom';
-import Home from './components/Home/Home.js';
-import CreateUser from './components/CreateUser/CreateUser.js';
-import ProfileHome from './components/ProfileHome/ProfileHome.js';
-import UserLogin from './components/UserLogin/UserLogin.js';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Route, Link } from "react-router-dom";
+import Home from "./components/Home/Home.js";
+import CreateUser from "./components/CreateUser/CreateUser.js";
+import ProfileHome from "./components/ProfileHome/ProfileHome.js";
+import UserLogin from "./components/UserLogin/UserLogin.js";
 
-let RootRoute = <Route path="/" render={()=>{
-  return Home;
-}}/>;
+let RootRoute = (
+  <Route
+    path="/"
+    render={() => {
+      return Home;
+    }}
+  />
+);
 function App() {
-
   return (
     <div className="App">
       <header>
-        <h1><i><Link exact={true} to="/">Tweeper</Link></i></h1>
-        <Link exact={true} to="/createUser">Link</Link>
-        <UserLogin/>
+        <div className="header-space">
+          <h1>
+            <i>
+              <Link exact={true} to="/" className="h1">
+                Tweeper
+              </Link>
+            </i>
+          </h1>
+          <Link exact={true} to="/createUser">
+            Link
+          </Link>
+          <UserLogin />
+        </div>
       </header>
       <main>
-      <Route exact={true} path="/" render={()=>{
-        return <Home/>
-      }}/>
+        <Route
+          exact={true}
+          path="/"
+          render={() => {
+            return <Home />;
+          }}
+        />
 
-      <Route exact={true} path="/createUser" render={()=>{
-        return <CreateUser/>
-      }}/>
-      <Route exact path="/home" render={
-        (renderProps)=>{
-          return <ProfileHome {...renderProps}/>
-        //  return <ParkDetails {...this.state.currentPark}></ParkDetails>
-        }
-      }/>
+        <Route
+          exact={true}
+          path="/createUser"
+          render={() => {
+            return <CreateUser />;
+          }}
+        />
+        <Route
+          exact
+          path="/home"
+          render={renderProps => {
+            return <ProfileHome {...renderProps} />;
+            //  return <ParkDetails {...this.state.currentPark}></ParkDetails>
+          }}
+        />
       </main>
       <footer></footer>
     </div>
--- src/components/UserLogin/UserLogin.js
+++ src/components/UserLogin/UserLogin.js
@@ -1,83 +1,91 @@
 import Api from "../../Api.js";
-import React, {Component} from 'react';
+import React, { Component } from "react";
 import "./UserLogin.css";
-import { withRouter } from 'react-router-dom';
-
+import { withRouter } from "react-router-dom";
 
 let api = new Api();
 
-
-class UserLogin extends Component{
-  constructor(props){
+class UserLogin extends Component {
+  constructor(props) {
     super(props);
     this.state = {
       classList: "UserLogin",
-      handle:'',
-      loggedIn:false
+      handle: "",
+      loggedIn: false
     };
   }
 
-  componentDidMount(){}
+  componentDidMount() {}
 
-  componentDidUpdate(){}
+  componentDidUpdate() {}
 
-  componentWillUnmount(){}
+  componentWillUnmount() {}
 
-  updateHandle = (evt)=>{
+  updateHandle = evt => {
     this.setState({
-      handle:evt.target.value
-    })
-  }
+      handle: evt.target.value
+    });
+  };
 
   login = () => {
-    api.loginUser(this.state.handle)
-      .then(res=>{
-        console.log(res.data[0]);
-        this.goToProfile(res.data[0]);
-        this.setState({
-          loggedIn: true
-        })
-      })
-  }
+    api.loginUser(this.state.handle).then(res => {
+      console.log(res.data[0]);
+      this.goToProfile(res.data[0]);
+      this.setState({
+        loggedIn: true
+      });
+    });
+  };
 
   logout = () => {
     this.setState({
-      loggedIn:false
-    })
+      loggedIn: false
+    });
     this.goToRoot();
-  }
+  };
 
-  goToProfile = (props)=>{
+  goToProfile = props => {
     console.log("Clicked");
     this.props.history.push({
-      pathname:`/home`,
-      state: {data: props }
+      pathname: `/home`,
+      state: { data: props }
     });
-  }
+  };
 
-  goToRoot = ()=>{
+  goToRoot = () => {
     console.log("Clicked");
     this.props.history.push({
-      pathname:`/`
+      pathname: `/`
     });
-  }
+  };
 
-  render(){
-    let notLoggedIn = ( <div>
-                <input onChange={this.updateHandle} value={this.state.handle}
-                  placeholder="Tweeper Handle"
-                  ></input>
-                <button onClick={this.login}>Login</button>
-                </div>);
-    let loggedIn = (<div>
-                      <p>Logged In as {this.state.handle}</p>
-                      <button onClick={this.logout}>logout</button>
-                    </div>);
-    return(
-      <div className={this.state.classList}>
-        {this.state.loggedIn ? loggedIn: notLoggedIn}
+  render() {
+    let notLoggedIn = (
+      <div>
+        <input
+          onChange={this.updateHandle}
+          value={this.state.handle}
+          placeholder="Tweeper Handle"
+          className="login-input"
+        ></input>
+        <button onClick={this.login} className="login-button">
+          Log in
+        </button>
       </div>
-        )
+    );
+    let loggedIn = (
+      <div>
+        <p>Logged In as {this.state.handle}</p>
+        <button onClick={this.logout} className="login-button">
+          Log out
+        </button>
+      </div>
+    );
+    return (
+      <div className={this.state.classList}>
+        {this.state.loggedIn ? loggedIn : notLoggedIn}
+      </div>
+    );
   }
 }
 
