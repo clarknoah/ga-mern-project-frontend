0d4f43f5a53bda27831cf6d8087a90d65b3776d0
Jenna Dean <jenna.dean9@gmail.com>
2019-11-21 13:38:43 -0500

    styling tweep stream

--- src/components/ReadTweep/ReadTweep.js
+++ src/components/ReadTweep/ReadTweep.js
@@ -1,12 +1,11 @@
-
-import React, {Component} from 'react';
+import React, { Component } from "react";
 import "./ReadTweep.css";
 import ReadComment from "../ReadComment/ReadComment";
 import CreateComment from "../CreateComment/CreateComment";
 import Api from "../../Api";
 let api = new Api();
-class ReadTweep extends Component{
-  constructor(props){
+class ReadTweep extends Component {
+  constructor(props) {
     super();
     let tweep = props.tweep;
     let date = new Date(tweep.timestamp);
@@ -14,79 +13,85 @@ class ReadTweep extends Component{
     let comments = this.sortByDate(tweep.comments);
     this.state = {
       classList: "ReadTweep",
-      tweep:props.tweep,
+      tweep: props.tweep,
       user: props.user,
-      comments:comments
+      comments: comments
     };
   }
 
-sortByDate = (arr)=>{
-  arr = arr.sort((a,b)=>{
-     a = new Date(a.timestamp);
-     b = new Date(b.timestamp);
-    return b - a;
-  })
-  return arr;
-}
+  sortByDate = arr => {
+    arr = arr.sort((a, b) => {
+      a = new Date(a.timestamp);
+      b = new Date(b.timestamp);
+      return b - a;
+    });
+    return arr;
+  };
 
-formatDate=(date)=>{
-  var monthNames = [
-    "January", "February", "March",
-    "April", "May", "June", "July",
-    "August", "September", "October",
-    "November", "December"
-  ];
+  formatDate = date => {
+    var monthNames = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December"
+    ];
 
-  var day = date.getDate();
-  var monthIndex = date.getMonth();
-  var year = date.getFullYear();
+    var day = date.getDate();
+    var monthIndex = date.getMonth();
+    var year = date.getFullYear();
 
-  return day + ' ' + monthNames[monthIndex] + ' ' + year;
-}
+    return day + " " + monthNames[monthIndex] + " " + year;
+  };
 
-  commentCreated=()=>{
+  commentCreated = () => {
     console.log("The Comment was created");
     let handle = this.state.user.handle;
     let tweepId = this.state.tweep._id;
-    api.readTweep(handle, tweepId)
-      .then(res=>{
-        console.log("New Comments", res);
-        let comments = this.sortByDate(res.data.comments);
-        this.setState({
-          comments:comments
-        })
-      })
-  }
+    api.readTweep(handle, tweepId).then(res => {
+      console.log("New Comments", res);
+      let comments = this.sortByDate(res.data.comments);
+      this.setState({
+        comments: comments
+      });
+    });
+  };
 
-  updateTweep=()=>{
+  updateTweep = () => {};
 
-  }
+  componentDidMount() {}
 
-
-
-  componentDidMount(){}
-
-  componentDidUpdate(){
+  componentDidUpdate() {
     console.log("Read Tweep Updated");
   }
 
-  componentWillUnmount(){}
+  componentWillUnmount() {}
 
-  render(){
-    let comments = this.state.comments.map(val=>{
-      return <ReadComment key={val._id} data={val}/>
-    })
-    return(
+  render() {
+    let comments = this.state.comments.map(val => {
+      return <ReadComment key={val._id} data={val} />;
+    });
+    return (
       <div className={this.state.classList}>
         <div className="tweepHeader">
-          <h6>{this.state.user.handle}| {`${this.state.tweep.timestamp} `}</h6>
+          <h6 className="handle-info">
+            {this.state.user.handle}| {`${this.state.tweep.timestamp} `}
+          </h6>
         </div>
-        <p>{this.state.tweep.tweepContent}</p>
+        <p className="tweep-content">{this.state.tweep.tweepContent}</p>
         {comments}
         <CreateComment
           commentCreated={this.commentCreated}
           user={this.state.user}
-          tweep={this.state.tweep} />
+          tweep={this.state.tweep}
+        />
       </div>
     );
   }
