539b40b55b3f643f00c8c1fd33d629fe5315f8f1
Jenna Dean <jenna.dean9@gmail.com>
2019-11-20 22:27:47 -0500

    started create user

--- src/components/CreateTweep/CreateTweep.js
+++ src/components/CreateTweep/CreateTweep.js
@@ -1,62 +1,61 @@
-
-import React, {Component} from 'react';
+import React, { Component } from "react";
 import "./CreateTweep.css";
 import Api from "../../Api.js";
 
-const api = new Api ();
+const api = new Api();
 
-class CreateTweep extends Component{
-  constructor(props){
+class CreateTweep extends Component {
+  constructor(props) {
     super();
     console.log(props);
     this.state = {
       classList: "CreateTweep",
-      newTweep:"",
+      newTweep: "",
       user: props.user
     };
   }
 
-  componentDidMount(){}
+  componentDidMount() {}
 
-  componentDidUpdate(){}
+  componentDidUpdate() {}
 
-  componentWillUnmount(){}
+  componentWillUnmount() {}
 
-  updateValue=(evt)=>{
+  updateValue = evt => {
     this.setState({
       newTweep: evt.target.value
-    })
-  }
+    });
+  };
 
-  submitTweep=(evt)=>{
+  submitTweep = evt => {
     console.log("Submitting Tweep");
     let handle = this.state.user.handle;
     let tweep = {
-      tweepContent:this.state.newTweep,
-      timestamp:new Date()
-    }
-    api.createTweep(handle,tweep)
-      .then(res=>{
-        this.props.tweepCreated();
-        console.log("Tweep Created");
-        this.setState({
-          newTweep:""
-        });
-      })
+      tweepContent: this.state.newTweep,
+      timestamp: new Date()
+    };
+    api.createTweep(handle, tweep).then(res => {
+      this.props.tweepCreated();
+      console.log("Tweep Created");
+      this.setState({
+        newTweep: ""
+      });
+    });
+  };
 
-  }
-
-
-
-  render(){
-    return(
+  render() {
+    return (
       <div className={this.state.classList}>
-        Create a new Tweep!
-        <textarea
-        value={this.state.newTweep}
-        onChange={this.updateValue}
-        ></textarea>
-        <button onClick={this.submitTweep}>Submit</button>
+        <div className="create-tweep">
+          Create a new Tweep!
+          <textarea
+            value={this.state.newTweep}
+            onChange={this.updateValue}
+          ></textarea>
+          <button onClick={this.submitTweep} className="submit-tweep-button">
+            Submit
+          </button>
+        </div>
       </div>
     );
   }
--- src/components/UserInfo/UserInfo.js
+++ src/components/UserInfo/UserInfo.js
@@ -1,18 +1,18 @@
-
-import React from 'react';
+import React from "react";
 import "./UserInfo.css";
 
-const UserInfo = (props) => {
+const UserInfo = props => {
   let classList = `UserInfo`;
 
-
-  return(
+  return (
     <div className={classList}>
-      <h3>{props.firstName} {props.lastName}</h3>
+      <h3>
+        {props.firstName} {props.lastName}
+      </h3>
       <h5>@{props.handle}</h5>
       <p>{props.description}</p>
     </div>
   );
-}
+};
 
 export default UserInfo;
